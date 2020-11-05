2ca27b89c700e4322f4b2da917df105f9f2138e8
logeshsivakumar <51087671+logeshsivakumar@users.noreply.github.com>
2020-11-05 16:44:44 +0530

    first commit

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 const kBottomContainerHeight=75.0;
-const kActiveCardColor= Color(0xFF1D1E33);
+const kActiveCardColor= Color(0xFF1D1E33)
 const kInactiveCardColor=Color(0xFF111328);
 const kBottomContainerColor= Color(0xFDD50000);
 
